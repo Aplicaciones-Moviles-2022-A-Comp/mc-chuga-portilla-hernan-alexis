80966f90ef936363b85225c8abc04173914a2e57
Hernan Alexis Chuga Portilla <hernan.chuga@epn.edu.ec>
2022-07-10 17:33:18 -0500

    Clase13

---
+++ Clase10-11-12-13/app/src/main/java/com/example/movcomphacp2022a/CRUD_Entrenador.kt
@@ -0,0 +1,11 @@
+package com.example.movcomphacp2022a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CRUD_Entrenador : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crud_entrenador)
+    }
+}
